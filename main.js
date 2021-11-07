149a14f7b686f243f7a51741e88cb9ba494e37f2
Diogo Santos <diogo_goncalves2004@hotmail.com>
2021-11-07 17:23:43 +0000

    making the bot save all commands automatically

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
+//Log the current version of Node.js
 console.log("Node.js version: " + process.version);
 
 // Require the necessary discord.js classes
-require('dotenv').config();
-const fs = require('fs');
-const { Client, Collection} = require('discord.js');
+require('dotenv').config();                          //Requiring enviromental variables for HEROKU
+const fs = require('fs');                            //File Stream
+const { Client, Collection} = require('discord.js'); //Discord Library
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
 //retrieve token from enviroment variables
 const token = process.env.TOKEN;
+const guildId = process.env.GUILD_ID;
+const clientId = process.env.CLIENT_ID;
 
 // Create a new client instance
 const client = new Client({
@@ -30,31 +35,49 @@ const client = new Client({
     ], 
 });
 
-//reading event files
+//Filter the event files path in an array
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
+//Get the events up and running so they activate when needed
 for (const file of eventFiles) {
 	const event = require(`./events/${file}`);
 	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
+		client.once(event.name, (...args) => event.execute(...args));   //set the events on the bot so it knows where they are (event, location)
 	} else {
-		client.on(event.name, (...args) => event.execute(...args));
+		client.on(event.name, (...args) => event.execute(...args));     //set the events on the bot so it knows where they are (event, location)
 	}
 }
 
-//reading command files
+//Set the slash commands of the bot
 client.commands = new Collection();
+
+//Put all commands in an array
+const commands = []
+//Put all command files path in an array
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-//registering existent commands
+//Get the commands up and running so they activate when needed
 for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
-    // Set a new item in the Collection
-	// With the key as the command name and the value as the exported module
-    client.commands.set(command.data.name, command);
+    const command = require(`./commands/${file}`);      
+    client.commands.set(command.data.name, command);    //set the commands on the bot so it knows where they are (command, location)
+    commands.push(command.data.toJSON());
 }
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+//Register commands
+(async () => {
+	try {
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully registered application commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
 
 // Login to Discord with your client's token
 client.login(token);
-
-module.exports = client
